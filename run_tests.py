07d69ba475504a7a98db3fe5769f4a4a677bf473
Mark Edgington <edgimar@gmail.com>
2012-10-27 15:52:24 -0400

    - updated test to use trailing newline

--- run_tests.py
+++ run_tests.py
@@ -10,12 +10,14 @@ class TestMichel(unittest.TestCase):
         pass
     
     def test_text_to_tasktree(self):
+        # text should have trailing "\n" character, like most textfiles
         org_text = """\
 * Headline 1
 Body 1a
 Body 1b
 * DONE Headline 2
-** Headline 2.1"""
+** Headline 2.1
+"""
         
         tasktree = m.parse_text(org_text)
         self.assertEqual(org_text, str(tasktree))
