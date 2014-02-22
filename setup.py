48f96d10017609873402cf8fbff6d28b082ad823
Mark Edgington <edgimar@gmail.com>
2014-02-22 16:29:32 -0500

    - update setup.py to require pyxdg - added idea of multi-list sync option to todo-list

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='michel',
-    version='0.0.6',
-    author='Christophe-Marie Duquesne',
-    author_email='chm.duquesne@gmail.com',
+    name='michel-orgmode',
+    version='0.1.0',
+    maintainer='Mark Edgington',
+    maintainer_email='edgimar@gmail.com',
     packages=find_packages(),
-    url='https://github.com/chmduquesne/michel',
+    url='https://bitbucket.org/edgimar/michel-orgmode',
     license=open('LICENSE.txt').read(),
-    description='pushes/pull flat text files to google tasks',
+    description='push/pull an org-mode file to a google-tasks task-list',
     long_description=open('README.md').read(),
-    install_requires = ['google-api-python-client'],
+    install_requires = ['google-api-python-client', 'pyxdg'],
     entry_points=("""
     [console_scripts]
-    michel = michel.michel:main
+    michel-orgmode = michel.michel:main
     """)
 )
